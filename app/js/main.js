37e0801bffc017ce29c7d816024112cc308d923b
Maxnx <helicopter0760@gmail.com>
2022-04-10 02:09:13 +0300

    Внёс исправления по 13 дз

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,49 @@
 $(function () {
 
-  
-  $(".cart__clean-btn").on('click', function () { 
+
+  $(".cart__clean-btn").on('click', function () {
     $(".cart__items").children().remove();
-  })
+  });
 
-  $(".product__close-btn").on('click', function () { 
+  $(".product__close-btn").on('click', function () {
     this.closest(".cart__item").remove();
-  })
+  });
 
   $(".cart__close-btn").on('click', function () {
     $(".cart").removeClass("cart--active");
     $(".wrapper").removeClass("wrapper--overlay");
-  })
+  });
 
   $(".cart__close-btn").on('click', function () {
     $(".cart").removeClass("cart--active");
     $(".wrapper").removeClass("wrapper--overlay");
-  })
+  });
 
   $('.dropdown').on('click', function () {
     $(".dropdown").toggleClass("dropdown--active");
-  })
+  });
 
   $('.user-nav__link--cart').on('click', function () {
     $(".cart").addClass("cart--active");
     $(".wrapper").addClass("wrapper--overlay");
-  })
+  });
 
 
   $('.slider__inner').slick({
     infinite: false,
     prevArrow: '<button type="button" class="slick-prev"><svg width="19" height="32"><use xlink:href="images/sprite.svg#icon-left-arrow"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg width="19" height="32"><use xlink:href="images/sprite.svg#icon-right-arrow"></use></svg></button>'
+  });
+
+  $('.brands__inner').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: '0',
   });
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
